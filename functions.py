60d60e669216e9e762b3a6d383cd7c2175b1bdc1
haydnllh <haydnllh719@gmail.com>
2024-09-10 18:07:10 +0100

    Adding regularisation and some changes to the  drawing function

--- functions.py
+++ functions.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import textwrap
 
-def draw_x_gametimes(df):
-    fig, ax = plt.subplots(figsize=(20, 10))
-    ax.bar([textwrap.fill(game, 10) for game in df.iloc[:, 1]], df.iloc[:, 2])
-    ax.set_xlabel('Game')
-    ax.set_ylabel('Playtime(log2)')
-    ax.set_yscale('log', base=2)
-    ax.tick_params(axis='x', labelsize=9)
+def draw_x_gametimes(*args):
+    fig, axs = plt.subplots(len(args), 1, figsize=(20, 10))
+    for ax, df in zip(axs, args):
+        ax.bar([textwrap.fill(game, 10) for game in df.iloc[:, 1]], df.iloc[:, 2])
+        ax.set_xlabel('Game')
+        ax.set_ylabel('Playtime(log2)')
+        ax.set_yscale('log', base=2)
+        ax.tick_params(axis='x', labelsize=9)
     plt.show()
 
 def draw_history(x, history):
